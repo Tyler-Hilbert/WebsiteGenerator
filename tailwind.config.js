02c4f5e71f458cdfdeb526d6192452fe9796df35
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2023-07-05 11:59:32 -0400

    FINALLY a working tailwind build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        "bookmark-purple": "#5d3694",
-        "bookmark-red": "#FA5959",
-        "bookmark-blue": "#242A45",
-        "bookmark-grey": "#9194A2",
-        "bookmark-white": "#f7f7f7",
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
       },
     },
-    fontFamily: {
-      Poppins: ["Poppins, sans-serif"],
-    },
-    container: {
-      center: true,
-      padding: "1rem",
-      screens: {
-        lg: "1124px",
-        xl: "1124px",
-        "2xl": "1124px",
-      },
-    },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
-};
+}
